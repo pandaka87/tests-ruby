e40555e582e2340e738131b758bb97ab08ce3949
pandaka87 <noellie.peuch2@gmail.com>
2022-07-11 15:49:41 +0200

    Exo 00 fini + Ajout de fichiers

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,25 @@
 def hello
+  return "Hello!"
+end
 
+hello
+
+def ask_name
+  puts "What's your name ?"
+  print " > "
+  name = gets.chomp
+  return name
 end
 
 def greet(name)
-
+  puts "Hello, #{name}!"
+  return "Hello, #{name}!"
 end
+
+def perform 
+  puts hello
+  name = ask_name
+  greet(name)
+end
+
+perform
---
+++ lib/01_temperature.rb
@@ -0,0 +1,41 @@
+def ask_u
+  puts "Quelle mesure souhaites-tu convertir ? (C ou F)"
+  puts "°C - Celsius"
+  puts "°F - Farhenheit"
+  print " > "
+  unite = gets.chomp
+  return unite
+end
+
+def ask_t
+  puts "Température à convertir :"
+  print " > "
+  temperature = gets.chomp.to_f
+  return temperature
+end
+
+def ftoc(temperature)
+  c = (temperature - 32) * 5/9
+  return c
+
+end
+
+def ctof(temperature)
+    f = (temperature * 9/5) + 32
+    return f
+end
+
+def perform
+  unite = ask_u
+  temperature = ask_t
+  case unite
+    when "F"
+      puts ftoc(temperature)
+    when "C"
+      puts ctof(temperature)
+    else 
+      puts "L'entrée n'est pas correcte. Veuillez indiquer C ou F."
+  end
+end
+
+perform
